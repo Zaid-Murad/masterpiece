dbd5847eff792372f8eeb043b84e2243707ca3d1
Zaid-Murad <119626142+Zaid-Murad@users.noreply.github.com>
2023-01-02 14:05:04 +0300

    Create codeforces2.cpp

---
+++ codeforces2.cpp
@@ -0,0 +1,43 @@
+    #include <iostream>
+    #include <vector>
+    #include <string>
+    // #include <boost/lexical_cast.hpp>
+     
+    void add(std::vector<std::vector<bool>>& vec, std::string& first, std::string& second)
+    {
+    	bool digit;
+     
+    	for (int x = 0; x < first.size(); x++)
+    	{
+    		std::string s(1, first[x]);
+    		digit = (bool)(std::stoi(s));
+    		vec[0].push_back(digit);
+    	}
+     
+     
+    	for (int x = 0; x < second.size(); x++)
+    	{
+    		std::string s(1, second[x]);
+    		digit = (bool)(std::stoi(s));
+    		vec[1].push_back(digit);
+    	}
+     
+    }
+     
+    int main()
+    {
+     
+    	std::vector<std::vector<bool>> vec(2);
+    	std::string first;
+    	std::string second;
+     
+    	std::cin >> first >> second;
+     
+    	add(vec, first, second);
+     
+    	
+    	for (int i = 0; i < vec[0].size(); i++)
+    		std::cout << !((vec[0][i] && vec[1][i]) || (!vec[0][i] && !vec[1][i]));
+    	
+     
+    }
